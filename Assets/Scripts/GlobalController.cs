05ab6073c2413e65d4c4c9bfb2f35effcea862cd
IFofPlanetune <nicolaj.oschegow@gmail.com>
2020-02-01 10:22:13 +0100

    test bulb works

--- Assets/Scripts/GlobalController.cs
+++ Assets/Scripts/GlobalController.cs
@@ -5,14 +5,35 @@ using UnityEngine;
 public class GlobalController : MonoBehaviour
 {
     // Start is called before the first frame update
+    public bool current = false;
+    public float clock_timer = 0.1f;
+    private float timer = 0f;
+
+    //debug things
+    public Light test_light;
+
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
+        if(timer >= clock_timer)
+        {
+            current = !current;
+            timer = Mathf.Max(0f, timer - clock_timer);
+
+            if(current)
+            {
+                test_light.SetLight(true);
+            }
+            else
+            {
+                test_light.SetLight(false);
+            }
+        }
     }
 }
---
+++ Assets/Scripts/Light.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Light : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private bool on;
+    public Sprite onSprite;
+    public Sprite offSprite;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetLight(bool b)
+    {
+        on = b;
+        if(b)
+        {
+            this.GetComponent<SpriteRenderer>().sprite = onSprite;
+        }
+        else
+        {
+            this.GetComponent<SpriteRenderer>().sprite = offSprite;
+        }
+    }
+}
